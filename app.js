55a00ae40e80b61a123f0879676c2adac1b5c34f
fassa108 <sambndeyefassa@gmail.com>
2026-05-31 21:09:47 +0000

    ajout de l'affichage des idees

--- app.js
+++ app.js
@@ -69,3 +69,52 @@ btnSoumettre.addEventListener("click", () => {
     afficherMessage("Idée publiée avec succès !", "success");
 });
 
+const couleurs = {
+    "Pédagogie": "#0d6efd",
+    "Événement": "#198754",
+    "Vie de campus": "#fd7e14",
+    "Amélioration technique": "#dc3545",
+};
+function afficherTout() {
+    mur.innerHTML = ""; // on vide avant de ré-injecter
+
+    if (idees.length === 0) {
+        messageVide.classList.remove("d-none");
+        compteur.textContent = "0 idée(s)";
+        return;
+    }
+
+    messageVide.classList.add("d-none");
+    compteur.textContent = `${idees.length} idée(s)`;
+
+    idees.forEach(idee => {
+        const col = document.createElement("div");
+        col.classList.add("col-md-6", "col-xl-4");
+
+        col.innerHTML = `
+            <div class="card h-100 shadow-sm border-0 carte-idee">
+                <div class="card-body d-flex flex-column">
+                    <div class="d-flex justify-content-between align-items-start mb-2">
+                        <span class="badge px-2 py-1" style="background-color: ${couleurs[idee.categorie] || '#6f2da8'}"> ${idee.categorie} </span>
+                    </div>
+                    <h5 class="card-title fw-bold mb-2">${idee.titre}</h5>
+                    <p class="card-text text-muted flex-grow-1">${idee.description}</p>
+                    <div class="d-flex gap-2 mt-3">
+                        <button class="btn btn-sm btn-outline-warning flex-grow-1" onclick="ouvrirEdition('${idee.id}')">
+                        <i class="bi bi-pencil me-1"></i>Modifier
+                        </button>
+                        <button class="btn btn-sm btn-outline-danger flex-grow-1" onclick="supprimerIdee('${idee.id}')">
+                            <i class="bi bi-trash me-1"></i>Supprimer
+                        </button>
+                    </div>
+                </div>
+            </div>
+        `;
+
+        mur.appendChild(col);
+    });
+}
+
+
+
+afficherTout();
